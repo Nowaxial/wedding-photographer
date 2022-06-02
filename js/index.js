065d21709673afd66538786d8de608d012f6d6d1
Nowaxial <maria.toledo@live.se>
2022-06-02 02:51:34 +0200

    Here we go once again

--- js/index.js
+++ js/index.js
@@ -1,40 +1,38 @@
 const cameraButton = document.querySelector('.button-div-camera');
 const videoElem  = document.querySelector('#camera');
 const takePictureButton = document.querySelector('.button-div');
-const canvas = document.querySelector('#picture');
-canvas.width = 342;
-canvas.height = 310;
 const galleryElem = document.querySelector('#gallery')
 const streamElem = document.querySelector('.stream-window');
 const pictureElem = document.querySelector('.picture-window')
-const newPictureElem = document.querySelector('.button-div-pic');
+const newPicturesElem = document.querySelector('.button-div-pic');
+
+
+const canvas = document.querySelector('#picture');
 const ctx = canvas.getContext('2d');
-const posterElem = document.querySelector('.poster');
+canvas.width = 342;
+canvas.height = 310;
 
 
 let stream;
 let images;
 
-let imagesFromStorage = JSON.parse(localStorage.getItem('weddingApp'))
-if(imagesFromStorage){
-    images = imagesFromStorage  // letar efter existerande bilder och lägger ihop nya + gamla
+let imagesStored = JSON.parse(localStorage.getItem('wedding-photos-App'))
+if (imagesStored) {
+    images = imagesStored
 } else {
-    images = []; // om inga bilder finns lägger vi till våra nya
+    images = [];
 }
-
 
 /* stream.html // Autostreamar kameran när behörighet ges*/
 async function cameraAutoStart(){
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) { // browser support
-            stream = await navigator.mediaDevices.getUserMedia({video:true});
+            stream = await navigator.mediaDevices.getUserMedia({video:true, audio:false});
             videoElem.srcObject = stream;
             videoElem.play();
     }
-}
-cameraAutoStart();
+};
 
 
-/* capture.html */
 takePictureButton.addEventListener ('click', () => {
     ctx.drawImage(videoElem, 0, 0, canvas.width, canvas.height);
     const imageData = canvas.toDataURL('image/png');
@@ -45,20 +43,19 @@ takePictureButton.addEventListener ('click', () => {
         id: images.lenght,
         image: imageData
     });
-    localStorage.setItem('weddingApp', JSON.stringify(images));
+    localStorage.setItem('wedding-photos-App', JSON.stringify(images));
 });
 
-newPictureElem.addEventListener ('click', () => {
-    streamElem.style.display='block' // göm videoDiv när vi tar bilden
+newPicturesElem.addEventListener ('click', () => {
+    streamElem.style.display='block' 
     pictureElem.style.display='none' 
-    takePictureButton.style.display = 'block'// visa pictureDiv när vi tar bilden
+    takePictureButton.style.display = 'block'
 });
 
-/* posterElem.addEventListener ('click', () => {
-    
-    pictureElem.style.display='none' 
-    
-}); */
+
+function newFunction() {
+    cameraAutoStart();
+}
 
 function createImage(image){
     const imageElem = document.createElement('img');
@@ -66,13 +63,15 @@ function createImage(image){
 
     galleryElem.append(imageElem);
 
-}
+};
 
 function getImages(){
     for (let image of images){
         createImage(image);
     }
 
-}
+};
 
+cameraAutoStart();
 getImages();
+
